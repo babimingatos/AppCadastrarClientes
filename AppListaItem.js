46bd76c0b6dfed83ae58b539ded0922a8820c5ea
Barbara Mingatos <72284498+babimingatos@users.noreply.github.com>
2021-06-23 21:08:49 -0300

    Update AppListaItem.js

--- AppListaItem.js
+++ AppListaItem.js
@@ -1,96 +1,96 @@
 import React from 'react';
-import {
-  Text,
-  View,
-  StyleSheet,
-  TextInput,
-  TouchableOpacity,
-  Alert,
-} from 'react-native';
+import {Text, View, StyleSheet, TextInput, TouchableOpacity, Alert} from 'react-native';
 import Database from './Database';
 
-async function handleEditarPress() {
-  const item = await Database.getItem(props.id); 
-  props.navigation.navigate(item);
+async function handleEditarPress(){
+  const item = await Database.getItem(props.id);
+  props.navigation.navigate("AppFormCliente", item);
 }
 
-function handleDeletePress() {
+function handleDeletePress(){
   Alert.alert(
-    'Atenção',
-    'Você deseja, realmente, excluir essa informação?',
+    "Atenção","Você deseja realmente excluir esta informação?",
     [
       {
-        text: 'Não',
-        onPress: () => console.log('Cancelado'),
-        style: 'cancel',
-      },
-      {
-        text: 'Sim',
-        onPress: () => {
-          Database.deleteItem(props.id).then((response) =>
-            props.navigation.navigate('AppLista', { id: props.id })
-          );
-        },
-      },
+      text:"Não",
+      onPress:() => console.log("cancelado"),
+      style: "cancel"
+    },
+    {
+      text:"Sim", onPress:() => {
+        Database.deleteItem(props.id).then(response => props.navigation.navigate("AppLista", {id: props.id}))
+      }
+    }
     ],
-    { cancelable: false }
+    {cancelable:false}
   );
 }
 
-export default function AppListaItem(props) {
-  return (
-    <View style={styles.container}>
-      <View style={styles.containerbotao}>
-        <TouchableOpacity style={styles.deletebotao}>
-          <Text style={styles.textobotao}>Excluir</Text>
-        </TouchableOpacity>
+export default function AppListaItem(props){
 
-        <TouchableOpacity style={styles.editarbotao}>
-          <Text style={styles.textobotao}>Editar</Text>
-        </TouchableOpacity>
-      </View>
+  return(
+      
+<View style={styles.container}>
+  <Text style={styles.textItem}>{props.item}</Text>
+    <View style={styles.containerbotao}>
+      <TouchableOpacity style={styles.deletebotao}>
+      <Text style={styles.textobotao}>Excluir</Text>
+      </TouchableOpacity>
+
+      <TouchableOpacity style={styles.editarbotao}>
+      <Text style={styles.textobotao}>Editar</Text>
+      </TouchableOpacity>
     </View>
-  );
+</View>
+ );
 }
 
 const styles = StyleSheet.create({
   container: {
-    backgroundColor: '#D2B48C',
-    marginTop: 30,
-    width: '100%',
+    backgroundColor:'#D2B48C',
+    marginTop:30,
+    width:'100%',
   },
 
   containerbotao: {
-    flexDirection: 'row-reverse',
+    flexDirection:'row-reverse',
     alignItems: 'flex-end',
     borderBottomWidth: 1,
-    borderBottonColor: 'black',
+    borderBottonColor:'black',
     paddingBottom: 10,
-    marginTop: 10,
+    marginTop:10,
   },
 
-  deletebotao: {
-    marginLeft: 10,
+  deletebotao:{
+    marginLeft:10,
     height: 60,
     width: 30,
     backgroundColor: 'red',
     borderRadius: 10,
-    fontSize: 12,
-    alignItems: 'center',
+    fontSize:12,
+    alignItems:'center',
   },
 
-  editarbotao: {
-    marginLeft: 10,
+
+
+  editarbotao:{
+    marginLeft:10,
     height: 60,
     width: 30,
     backgroundColor: 'yellow',
     borderRadius: 10,
-    fontSize: 12,
-    alignItems: 'center',
+    fontSize:12,
+    alignItems:'center',
   },
 
   textobotao: {
-    fontSize: 20,
-    color: 'black',
+    fontSize:20,
+    color:'black'
   },
+
+textItem:{
+  fontSize:16,
+}
+
 });
+
